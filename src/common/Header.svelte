d3b4a02b2ef029e6c1864f0dab03788138e82380
Amirthanathan R <amirth300324@gmail.com>
2024-03-30 15:45:49 +0530

    placed navbar in header

--- src/common/Header.svelte
+++ src/common/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { fly } from "svelte/transition";
+    import { fly, scale } from "svelte/transition";
     $: sticky = false;
     document.addEventListener('scroll', (e) => {
         sticky = window.scrollY > 800;
@@ -7,15 +7,24 @@
 
 </script>
 
+<header class="bg-gray-300/40 flex justify-between items-center absolute w-full px-12 py-4">
+<h1>
+    Aravind S
+</h1>
 {#key sticky}
-<nav in:fly={{y:sticky ? -200 : 0, duration: 300}} class:stk={sticky} class="left-1/2 -translate-x-1/2 absolute divide-x divide-gray-600 grid grid-flow-col w-fit rounded-full  bg-gray-200 top-3">
-    <div class="px-5 py-2">nav</div>
-    <div class="px-5 py-2">nav</div>
-    <div class="px-5 py-2">nav</div>
+<nav in:fly={{y:sticky ? -200 : 0, duration: 300}} out:scale class:stk={sticky} class="bg-transparent grid grid-flow-col w-fit bg-gray-200 top-3">
+    <div class="first:rounded-l-full last:rounded-r-full hover:scale-110 hover:bg-gray-400 bg-slate-300 p-3">nav</div>
+    <div class="first:rounded-l-full last:rounded-r-full hover:scale-110 hover:bg-gray-400 bg-slate-300 p-3">nav</div>
+    <div class="first:rounded-l-full last:rounded-r-full hover:scale-110 hover:bg-gray-400 bg-slate-300 p-3">nav</div>
 </nav>
 {/key}
+<div>
+    Contact Me
+</div>
+</header>
+
 <style>
     .stk {
-        @apply sticky top-3;
+        @apply  left-1/2 -translate-x-1/2 fixed top-5  backdrop-opacity-25 backdrop-blur divide-x px-2 py-1 divide-gray-600;
     }
 </style>
