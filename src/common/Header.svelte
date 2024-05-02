45f7b161c4bb6a2409a169d266a4f13d1a2fa093
Aravind-SL <aravind9180@gmail.com>
2024-05-02 17:44:12 +0530

    Temporary changes

--- src/common/Header.svelte
+++ src/common/Header.svelte
@@ -1,32 +1,46 @@
 <script lang="ts">
     import {link} from 'svelte-spa-router';
     import { fly, scale } from "svelte/transition";
+  import { div } from 'three/examples/jsm/nodes/Nodes.js';
     $: sticky = false;
     document.addEventListener('scroll', (e) => {
         sticky = window.scrollY > window.innerHeight / 4;
     });
 
+    let menuToggle = false;
+    let mdScreen = screen.width > 1024;
+
 </script>
 
-<header class="text-xs md:text-lg bg-gray-300/40 flex justify-between items-center absolute w-full px-12 py-4">
+<header class="px-8 py-6 z-10 absolute top-0 left-0 flex  lg:flex-row lg:justify-between w-screen flex-col items-center justify-center">
+
 <a href="/" use:link>
     <h1 class="text-2xl font-medium">
         Aravind S
     </h1>
 </a>
-{#key sticky}
-<nav in:fly={{y:sticky ? -200 : 0, duration: 300, delay: 200}} out:scale class:stk={sticky} class="bg-transparent grid grid-flow-col w-fit bg-gray-200 top-3">
-    <a transition:scale href="" class:stk-nav={sticky} class="hover:scale-110 hover:bg-gray-400 transition-transform  px-3 py-2">About Me</a>
-    <a transition:scale={{delay:100}} href="" class:stk-nav={sticky} class="hover:scale-110 hover:bg-gray-400 transition-transform px-3 py-2">Projects</a>
-    <a transition:scale={{delay:200}} href="" class:stk-nav={sticky} class="hover:scale-110 hover:bg-gray-400 transition-transform px-3 py-2">Certifications</a>
-    <a transition:scale={{delay:300}} href="" class:stk-nav={sticky} class="hover:scale-110 hover:bg-gray-400 transition-transform px-3 py-2">Extras</a>
-</nav>
-{/key}
-<div class="text-2xl inline-flex gap-4">
+
+<div class="text-2xl inline-flex gap-4 lg:order-last">
     <a class="hover:scale-125 transition transition-transform" href="mailto:aravind9180@gmail.com"><i class="fa-solid fa-at"></i></a>
     <a class="hover:scale-125 transition transition-transform" href="https://github.com/Aravind-sl"><i class="fa-brands fa-github"></i></a>
     <a class="hover:scale-125 transition transition-transform" href="https://linkedin.in/"><i class="fa-brands fa-linkedin"></i></a>
 </div>
+
+<button on:click={
+    () => {menuToggle = ! menuToggle}
+} class="p-2 text-gray-900 text-sm lg:hidden transition-transform duration-75 ease-in-out hover:scale-125 rounded-md  hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">
+    Menu
+</button>
+
+{#key sticky}
+<nav in:fly={{y:sticky ? -200 : 0, duration: 300, delay: 200}} out:scale class:stk={sticky} class="flex bg-gray-800/40 md:bg-transparent {mdScreen ? '' : menuToggle ? 'border-2 border-gray-900 m-4 animate-menu-drop flex-col text-center rounded-md items-center backdrop-blur-sm' : 'hidden'} text-sm justify-evenly md:gap-4  text-gray-900">
+    <a transition:scale href="" class:stk-nav={sticky} class="hover:scale-110 hover:bg-gray-400 transition-transform  px-3 py-2">About Me</a>
+    <a transition:scale={{delay:100}} href="" class:stk-nav={sticky} class="hover:scale-110 hover:bg-gray-400 transition-transform px-3 py-2">Projects</a>
+    <a transition:scale={{delay:200}} href="" class:stk-nav={sticky} class="hover:scale-110 hover:bg-gray-400 transition-transform px-3 py-2">Certifications</a>
+    <a transition:scale={{delay:300}} href="" class:stk-nav={sticky} class="hover:scale-110 hover:bg-gray-400 transition-transform px-3 py-2">Extras</a>
+
+</nav>
+{/key}
 </header>
 
 <style>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,36 +1,66 @@
 <script lang="ts">
-    import {fade} from 'svelte/transition';
-    import {typeWriter} from '../lib/CustomTransition';
 //     import {setup} from '../lib/PixiRenderer';
-    import {setup, animate} from '../lib/ThreeRenderer';
     import Typewriter from 'svelte-typewriter';
-    import { concurrent, loop } from 'svelte-typewriter'
+    import { onMount } from 'svelte';
+    import {Link} from 'svelte-routing';
+    let currentIndex = 0;
+    const names: string[] = [
+        'Data Engineer',
+        'Data Scientist',   
+        'ML Engineer',
+        'Web Developer'
+    ];
 
-  const messages: string[] = [
-    'Hello, world!',
-    'This is a looping typewriter animation.',
-    'Enjoy the show!'
-  ];
+    let message = names[currentIndex];
 
-  let currentIndex: number = 0;
-  const updateIndex = () => {
-    currentIndex = (currentIndex + 1) % messages.length;
-  };
+    onMount(() => {
+        const intervalId = setInterval(() => {
+        currentIndex = (currentIndex + 1) % names.length;
+        message = names[currentIndex];
+        }, 3200);
+
+        return () => clearInterval(intervalId);
+    });
 </script>
 
-
 <main id="main" class="w-full snap-y snap-mandatory z-10">
-    <section class="w-full flex justify-between items-center snap-center h-screen">
-        <Typewriter mode="scramble" interval={50} scrambleDuration={3000}>
-            <h1>HEllo i am aravind</h1>
-        </Typewriter>
-        
+    <!-- Hero Section -->
+    <section class ="w-full flex justify-between items-center snap-center h-screen lg:px-16">
+        <div class="flex flex-col">
+            <Typewriter mode="scramble" interval={50} scrambleDuration={2500}>
+                <h1 class="text-gray-900 text-4xl lg:text-7xl ">Hello I'm Aravind</h1>
+              </Typewriter>
+          
+              <Typewriter mode="loop" interval={100}>
+                <h1 class="flex flex-row items-end mt-4 text-gray-900 text-2xl lg:text-5xl ">I'm A <span class="text-gray-900 text-xl lg:text-3xl lg:ml-12">{message}</span></h1>
+              </Typewriter>
+        </div>
+        <div >
+            <!-- svelte-ignore a11y-img-redundant-alt -->
+            <img class="rounded-full" src="Profile.jpg" alt="Profile photo" height="500" width="500">
+        </div>
+    </section>
 
+    <!-- Quote Section -->
+    <section class="w-full flex justify-center items-center snap-center mt-16 mb-16">
+        <div class="flex flex-col rounded-md m-8 p-8 border-2 border-gray-400 w-full">
+            <p class="text-3xl text-balance">
+                “Computers are incredibly fast, accurate, and stupid. Human beings are incredibly slow, inaccurate, and brilliant. Together they are powerful beyond imagination.”
+            </p>
+            <p class="mt-8 text-2xl flex flex-row-reverse">
+                - Albert Einstein
+            </p>
+        </div>
     </section>
-    <section class="w-full flex justify-center items-center snap-center h-screen">
-        <p>Section 2</p>
-    </section>
-    <section class="w-full flex justify-center items-center h-screen bg-gray-300/40 snap-center">
-        <p>Section 3</p>
-    </section>
+
+    <!-- Project Section -->
+    <!-- <section class="w-full flex justify-center items-center h-screen snap-center">
+        <div class="flex items-center flex-wrap justify-center gap-8 px-32 mt-12">
+            <Link class="border-rose-400 border-2 group text-gray-200 h-48 py-10 px-4 w-64 md:w-96 flex gap-2 justify-center rounded-md items-center flex-col cursor-pointer hover:bg-slate-500/20" to="/event/{to}">
+                <span class="font-anta text-sm md:text-xl w-full text-wrap text-center group-hover:text-2xl text-wrap uppercase leading-tight ">{heading}</span>
+                <span class="font-kdam text-xs group-hover:text-lg md:text-md">sss</span>
+                <span class="font-kdam text-xs group-hover:text-lg md:text-md">sss</span>
+            </Link>
+        </div>
+    </section> -->
 </main>
